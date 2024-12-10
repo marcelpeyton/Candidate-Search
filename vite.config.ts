ab815b66c582f8865a9605c12e358b721ce3e0c0
Marcel Peyton <marcel-p@outlook.com>
2024-12-10 18:17:08 -0500

    added page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
-  base: "/Candidate-Search",
+  base: "/",
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
 });
